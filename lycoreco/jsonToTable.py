61a976928e2f8c5a69c251e4d848f8b1147cd0d4
Eda <rikuto.iida725@icloud.com>
2024-02-10 00:27:11 +0900

    .tsファイル出力先用にoutputフォルダの存在確認と作成

--- lycoreco/jsonToTable.py
+++ lycoreco/jsonToTable.py
@@ -1,9 +1,17 @@
 import json
 import csv
+import os
 
 radioTitle = "リコラジ"
 workTitle = "lycoreco"
 tail = ".ts"
+falder = "./output/"
+
+is_dir = os.path.isdir(falder)
+if is_dir:
+    print(f"{falder} is exist.")
+else:
+    os.mkdir(falder)
 
 my_Table = [["title","delivery_date","streaming_url","poster_image_url"]]
 
@@ -11,7 +19,7 @@ with open("{}.json".format(workTitle)) as f:
     D_json = json.load(f)
 
 for num in D_json["contents"]:
-    my_Table.append([radioTitle + num["title"] + tail, num["delivery_date"], num["streaming_url"], num["poster_image_url"]])
+    my_Table.append([falder + radioTitle + num["title"] + tail, num["delivery_date"], num["streaming_url"], num["poster_image_url"]])
 
 #print(my_Table)
 
