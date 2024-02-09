d40669b60d4e06eb6426b49363ec09bfdeda71f1
Eda <rikuto.iida725@icloud.com>
2024-02-09 20:08:38 +0900

    jsonDL

--- lycoreco/jsonDL.py
+++ lycoreco/jsonDL.py
@@ -7,7 +7,8 @@ url = "https://www.onsen.ag/web_api/programs/lycoris-recoil"
 
 # APIからデータを取得
 response = requests.get(url)
-
+print(type(response))
+print(type(response.json()))
 # JSON形式で保存
 with open("lycoreco.json", "w") as f:
-    json.dump(response.json(), f)
+    json.dump(response.json(), f, ensure_ascii=False)
