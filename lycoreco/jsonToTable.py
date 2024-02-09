d41a1e6619697f96b9fd5abe69be4e582645e7c2
Eda <rikuto.iida725@icloud.com>
2024-02-09 21:39:35 +0900

    first commit

---
+++ lycoreco/jsonToTable.py
@@ -0,0 +1,19 @@
+import json
+import csv
+
+radioTitle = "リコラジ"
+workTitle = "lycoreco"
+i = 0
+my_Table = [["title","delivery_date","streaming_url","poster_image_url"]]
+
+with open("{}.json".format(workTitle)) as f:
+    D_json = json.load(f)
+
+for num in D_json["contents"]:
+    my_Table.append([radioTitle + num["title"], num["delivery_date"], num["streaming_url"], num["poster_image_url"]])
+
+#print(my_Table)
+
+with open("{}.csv".format(workTitle), 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerows(my_Table)
